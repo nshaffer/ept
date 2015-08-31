f190067144aff97531da33e79cd938dca52ce562
nshaffer <nathaniel-shaffer@uiowa.edu>
2015-08-31 10:16:33 -0500

    Initial commit

---
+++ main.f90
@@ -0,0 +1,8 @@
+program main
+  use error, only: throw
+  use types, only: dp
+  use iotools, only: loadtxt, savetxt
+  use interp, only: cubic_coeff, cubic_eval
+  implicit none 
+  
+end program main
